0f15ddb74a31a9f53df6b4731a364a0cba021daf
Yenerson H.lopez <74636100+Apock-graficos@users.noreply.github.com>
2023-05-27 13:52:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Float chat whatsapp
+<a href="https://www.youtube.com/ApockGraficos"><img src="assets/youtube.svg"> Apock graficos</a>
+
+### implementation guide
+Copy HTML template and implement css and js XD
+
+```html
+<!-- css -->
+<link rel="stylesheet" type="text/css" href="float-chat-whatsapp.css">
+<!-- js -->
+<script type="text/javascript" src="float-chat-whatsapp.js"></script>
+```
+
+### Declare script
+
+```js
+const options = {
+    phone: 3000000000,      /* number phone */
+    phonePrefix: 10,        /* number phone prefix */
+    // position: 'end',     /* optional, 'end' | 'start' -> default 'end' */
+    // color: 'pink',       /* optional, 'green' | 'red' | 'blue' | 'orange' | 'pink' | 'purple' -> default 'green' */
+    // separe: 3            /* optional, 1 ~ 3 -> default 2 */
+}
+
+new FloatChatWhatsapp('float-chat-whatsapp', options)
+```
+
+### HTML guide
+guide to apply css selectors in case of not using js
+
+```html
+<div class="ag-float-chat-whatsapp [position] [separed] [theme]" id="float-chat-whatsapp">
+```
+
+| action      | option                                              | example       |
+| ----------- | --------------------------------------------------- | ------------- |
+| position    | `start` or `end`                                    | `start`       |
+| separed     | `separe-` + from 1 to 3                             | `separe-2`    |
+| color theme | `theme-` + `green, red, orange, blue, pink, purple` | `theme-green` |
+
+
+#### Use avatar or icon
+This goes inside the container with the `<div class="chat-header">` selector, you can be the first or last element
+
+```html
+<!-- icono -->
+<div class="item-icon md">
+    <i class="icon icon-whaptsapp"></i>
+</div>
+
+<!-- avatar -->
+<div class="avatar offline md">
+    <img src="..." alt="img-avatar">
+</div>
+```
+
+adjust icon, avatar and button size with:
+`sm` (small) or `md` (medium)
+
+### Tutorial Guide to implement with:
+
+<table>
+    <tr>
+        <td><img width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" /></td>
+        <td><img width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg" /></td>
+        <td><img width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" /></td>
+    </tr>
+    <tr>
+        <td><a href="https://www.youtube.com/ApockGraficos">Html and js</a></td>
+        <td><a href="https://www.youtube.com/ApockGraficos">Angular</a></td>
+        <td><a href="https://www.youtube.com/ApockGraficos">Vue</a></td>
+    </tr>
+</table>
+
---
+++ float-chat-whatsapp.js
@@ -0,0 +1,65 @@
+var FloatChatWhatsapp = /** @class */ (function () {
+    function FloatChatWhatsapp(el, options) {
+        if (options === void 0) { options = null; }
+        this.selectorDnone = 'display-none';
+        this.options = {
+            phone: options && options.phone ? options.phone : null,
+            phonePrefix: options && options.phonePrefix ? options.phonePrefix : null,
+            color: options && options.color ? options.color : 'green',
+            position: options && options.position ? options.position : 'end',
+            separe: options && options.separe ? options.separe : 1
+        };
+        console.log("this.options", this.options);
+        this.el = document.getElementById(el);
+        this.el.classList.add('theme-' + this.options.color);
+        this.el.classList.add('separe-' + this.options.separe);
+        this.el.classList.add(this.options.position);
+        this.exe();
+    }
+    FloatChatWhatsapp.prototype.exe = function () {
+        var _this = this;
+        var btnToggle = this.el.querySelectorAll('[data-btn="button-toggle"]');
+        var form = this.el.querySelector('form');
+        var chat = this.el.querySelector('.chat-container');
+        var subtitle = this.el.querySelector('.subtitle');
+        var buttonSubmit = this.el.querySelector('[type="submit"]');
+        // button toggle open-close
+        btnToggle.forEach(function (btn) { return btn.addEventListener('click', function () {
+            chat.classList.toggle(_this.selectorDnone);
+            btnToggle[0].classList.toggle('effect-pause');
+        }); });
+        // subtilte
+        subtitle.innerText = this.formatPhoneNumber();
+        // form
+        form.addEventListener('input', function () {
+            var inputName = form.querySelector('[name="name"]').value;
+            var inputMessage = form.querySelector('[name="message"]').value;
+            inputName.length > 0 && inputMessage.length > 0 ? buttonSubmit.removeAttribute('disabled') : buttonSubmit.setAttribute('disabled', 'true');
+        });
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            var formControl = new FormData(e.target);
+            var message = "".concat(formControl.get('message'), ". ATT: ").concat(formControl.get('name'));
+            if (_this.options.phone && _this.options.phonePrefix) {
+                var time_1 = setTimeout(function () {
+                    window.open("https://api.whatsapp.com/send?phone=+".concat(_this.options.phonePrefix).concat(_this.options.phone, "&text=").concat(message), '_blank');
+                    clearTimeout(time_1);
+                    e.target.reset();
+                    chat.classList.add(_this.selectorDnone);
+                }, 500);
+            }
+            else {
+                alert('UPS!. phone number or/and prefix not assigned');
+            }
+        });
+    };
+    FloatChatWhatsapp.prototype.formatPhoneNumber = function () {
+        var cleaned = ('' + this.options.phone).replace(/\D/g, '');
+        var match = cleaned.match(/^(.{3})(.{3})(.{4})$/);
+        if (match) {
+            return "(".concat(match[1], ") ").concat(match[2], "-").concat(match[3]);
+        }
+        return '';
+    };
+    return FloatChatWhatsapp;
+}());
